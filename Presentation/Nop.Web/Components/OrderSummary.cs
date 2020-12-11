49d6d92297613fea7d3ee7c9744ae06267209202
lalvaro90 <lalvaro90@gmail.com>
2020-12-11 14:28:24 -0600

    Add project files.

---
+++ Presentation/Nop.Web/Components/OrderSummary.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Orders;
+using Nop.Services.Catalog;
+using Nop.Services.Orders;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+using Nop.Web.Models.ShoppingCart;
+
+namespace Nop.Web.Components
+{
+    public class OrderSummaryViewComponent : NopViewComponent
+    {
+        private readonly IShoppingCartModelFactory _shoppingCartModelFactory;
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IStoreContext _storeContext;
+        private readonly IWorkContext _workContext;
+        private readonly IOrderService _orderService;
+        private readonly IProductService _productService;
+
+        public OrderSummaryViewComponent(IShoppingCartModelFactory shoppingCartModelFactory,
+            IShoppingCartService shoppingCartService,
+            IStoreContext storeContext,
+            IWorkContext workContext, IOrderService orderService, IProductService productService)
+        {
+            _productService = productService;
+            _orderService = orderService;
+            _shoppingCartModelFactory = shoppingCartModelFactory;
+            _shoppingCartService = shoppingCartService;
+            _storeContext = storeContext;
+            _workContext = workContext;
+        }
+
+        public IViewComponentResult Invoke(bool? prepareAndDisplayOrderReviewData, ShoppingCartModel overriddenModel)
+        {
+            //use already prepared (shared) model
+            if (overriddenModel != null)
+                return View(overriddenModel);
+
+            //if not passed, then create a new model
+            var cart = _shoppingCartService.GetShoppingCart(_workContext.CurrentCustomer, ShoppingCartType.ShoppingCart, _storeContext.CurrentStore.Id);
+
+            var model = new ShoppingCartModel(_workContext, _orderService, _productService, _shoppingCartService);
+            model = _shoppingCartModelFactory.PrepareShoppingCartModel(model, cart,
+                isEditable: false,
+                prepareAndDisplayOrderReviewData: prepareAndDisplayOrderReviewData.GetValueOrDefault());
+            return View(model);
+        }
+    }
+}
